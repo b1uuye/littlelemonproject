5ba7aeecf02a957ae40f9b530b18fa4ec9e7d144
Israel Onibudo <israelonibudo@Israels-MacBook-Pro.local>
2024-05-20 15:06:24 +0100

    started responsive view

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../images/logosmall.jpg';
 import './Header.css';
 import { Link as LinkScroll } from 'react-scroll';
 import { Link } from 'react-router-dom';
+import burger from '../images/more.png';
+
 
 const Header = () => {
+  const [isOpen,setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+  setIsOpen ((open) => !open);
+  };
+
   return (
     <div className='head-container' id='header'>
         <div className='head-left'>
@@ -13,7 +21,8 @@ const Header = () => {
             </Link>
         </div>
         <div className='head-right'>
-            <ul className='nav_menu'>
+          <li className='menu_drop'><img src={burger} alt="menudrop" onClick={toggleMenu} /></li>
+            <ul className={`nav_menu ${isOpen ? "is-open" : ''}`}>
                 <LinkScroll to='hero' span={1} smooth={true} duration={500} style={{cursor: 'pointer'}}><li>Home</li></LinkScroll>
                 <LinkScroll to='about' span={1} smooth={true} duration={500} style={{cursor: 'pointer'}}><li>About</li></LinkScroll>
                 <LinkScroll to='specials' span={1} smooth={true} duration={500} style={{cursor: 'pointer'}}><li>Menu</li></LinkScroll>
